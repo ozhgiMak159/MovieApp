3f94c62e7a6bf1bf4a7f64cfbbacabec83efc207
Maksim Ozhgibecov <93679282+ozhgiMak159@users.noreply.github.com>
2022-07-02 15:48:15 +0500

    SetupTabBar

---
+++ MovieApp/Controllers/MainTabBar/MainViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MainViewController.swift
+//  MovieApp
+//
+//  Created by Maksim  on 02.07.2022.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        setupTabBar()
+        setupItems()
+    }
+    
+    private func setupTabBar() {
+        tabBar.backgroundColor = #colorLiteral(red: 0.1411685311, green: 0.1457938763, blue: 0.1374607567, alpha: 1)
+        tabBar.tintColor = .white
+        tabBar.unselectedItemTintColor = #colorLiteral(red: 0.3392201259, green: 0.3392201259, blue: 0.3392201259, alpha: 1)
+    }
+    
+    private func setupItems() {
+        let homeVc = UINavigationController(rootViewController: HomeViewController())
+        let upcomingVc = UINavigationController(rootViewController: UpcomingViewController())
+        let searchVc = UINavigationController(rootViewController: SearchViewController())
+        let downloadVc = UINavigationController(rootViewController: DownloadsViewController())
+        
+        setViewControllers([homeVc, upcomingVc, searchVc,downloadVc], animated: true)
+        
+        homeVc.tabBarItem.image = UIImage(systemName: "house")
+        upcomingVc.tabBarItem.image = UIImage(systemName: "play.circle")
+        searchVc.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        downloadVc.tabBarItem.image = UIImage(systemName: "square.and.arrow.down")
+        
+        homeVc.title = "Home"
+        upcomingVc.title = "Upcoming"
+        searchVc.title = "Top Search"
+        downloadVc.title = "Downloads"
+        
+    }
+
+
+}
+
