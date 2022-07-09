aebf6d1d499dd0d072d982799add4256f69d1083
Maksim Ozhgibecov <93679282+ozhgiMak159@users.noreply.github.com>
2022-07-09 10:14:03 +0500

    1

--- MovieApp/Controllers/TitlePreviewViewController.swift
+++ MovieApp/Controllers/TitlePreviewViewController.swift
@@ -19,7 +19,8 @@ class TitlePreviewViewController: UIViewController {
     private var imagePoster: UIImageView = {
        let imageView = UIImageView()
         imageView.image = UIImage(named: "Стетем")
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .top
+        imageView.clipsToBounds = true
        // imageView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
@@ -32,6 +33,10 @@ class TitlePreviewViewController: UIViewController {
         view.backgroundColor = .systemBackground
         setAddView()
         setConstraint()
+        
+//       // imagePoster.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height / 2)
+//        print(imagePoster.frame.height)
+//        print(imagePoster.frame.width)
     }
     
     private func setupGradient() {
@@ -39,13 +44,13 @@ class TitlePreviewViewController: UIViewController {
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [
             UIColor.clear.cgColor,
-            UIColor.systemBackground.cgColor
+            UIColor.systemBackground.cgColor,
         ]
     }
     
     private func setAddView() {
         view.addSubview(imagePoster)
-        setupGradient()
+      //  setupGradient()
     }
     
     private func setConstraint() {
@@ -53,7 +58,10 @@ class TitlePreviewViewController: UIViewController {
             imagePoster.topAnchor.constraint(equalTo: view.topAnchor),
             imagePoster.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             imagePoster.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            imagePoster.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -250),
+           // imagePoster.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -300),
+          //  imagePoster.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6),
+            imagePoster.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5, constant: 70)
+          
            
         ])
     }
